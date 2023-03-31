fc8ae57ac5058f7da269046e0c639e4359956680
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-03-31 23:45:37 +0200

    TK admin panel

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, PriceDisplay
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+    list_display = ('id', 'name', 'price', 'last_modified')
+
+
+class PriceDisplayAdmin(admin.ModelAdmin):
+    model = PriceDisplay
+
+    list_display = ('id', 'ip_address')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(PriceDisplay, PriceDisplayAdmin)
